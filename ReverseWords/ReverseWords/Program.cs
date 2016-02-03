b5bce25a39caa9ed4a2ce677b80232f6cfed0bab
Ghebre <geberhane@gmail.com>
2016-02-03 12:54:01 -0800

    Reverse words in a sentence.
    *Create a method which takes in a string.
    *Use argument.split(' ') to put the words apart
    *Put the result in an an array
    *Use Array.Reverse(argument)
    *return string.Join(' ', name of array)

---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.reverseSentence("My name is Ghebre"));
+            Console.ReadLine();
+        }
+
+        public static string reverseSentence(string sent)
+        {
+            //a method that takes in a string and reverses the words as an output
+            string[] words = sent.Split(' ');
+            Array.Reverse(words);
+
+            return string.Join(" ", words);
+        }
+             
+            
+
+         
+    }
+}
